e95e6dd0c6d1a3a8a385a48f00e1203e1d072241
RabijanR <rabijan.ragupalan@ontariotechu.net>
2025-11-11 14:11:44 -0500

    Created basic pages

--- Lost-and-Found-Hub/routes/index.js
+++ Lost-and-Found-Hub/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+router.get('/reportit', function(req, res, next) {
+  res.render('index', { title: 'Report it' });
+});
+
+router.get('/viewall', function(req, res, next) {
+  res.render('index', { title: 'View All' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'How it Works' });
+});
+
+router.get('/contactus', function(req, res, next) {
+  res.render('index', { title: 'Contact us' });
 });
 
 module.exports = router;
