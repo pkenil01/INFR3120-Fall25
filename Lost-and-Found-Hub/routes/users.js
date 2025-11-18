f1b6efa40bb9d69195969753bcc22d5bb4091033
pkenil01 <patelkenil2312@gmail.com>
2025-11-18 11:17:40 -0500

    adding routes to register page

--- Lost-and-Found-Hub/routes/users.js
+++ Lost-and-Found-Hub/routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Kenil Part 2 - Users Route File */
+
+/* Import the user model */
+var user = require('../models/user');
+
+/* Get users registration page. */
+router.get('/register', function(req, res, next) {
+  res.render('users/register', { title: 'Register' });
+});
+
+/* Post users registration page. */
+router.post('/register', function(req, res, next) {
+  /* Get user data */
+  let newUser = new user({
+    username: req.body.username,
+    password: req.body.password
+  });
+  /* Save user data to the database */
+  newUser.save(function(err) {
+    if (err) {
+      /* Handle registration error */
+      return res.send('Error registering user.');
+    }
+    /* Redirect to login page after successful registration */
+    res.send('User registered successfully. Please <a href="/users/login">login</a>.');
+  });
+});
+
+
+
+/*Code from Part1 /*
+/* 
+GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+*/
 
 module.exports = router;
