bd700ede1e02a6b81ba674dd5e8b9e0278f86cd6
RabijanR <rabijan.ragupalan@ontariotechu.net>
2025-11-19 12:37:26 -0500

    fixxed navbar to update depending on the webpage with possible issues

--- Lost-and-Found-Hub/config/app.js
+++ Lost-and-Found-Hub/config/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 let mongoose = require('mongoose');
 let DB = require('./db');
 
-// mongoose connection
+const session = require('express-session'); // sessions
+
 mongoose.connect(DB.URI);
 let mongoDB = mongoose.connection;
 mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
@@ -31,9 +32,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 
-// Makes "user" available to all EJS views (prevents undefined error)
+// session middleware
+app.use(session({
+  secret: 'lostandfoundsecret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// make user available to all EJS files
 app.use((req, res, next) => {
-  res.locals.user = null; 
+  res.locals.user = req.session.user || null;
   next();
 });
 
@@ -41,7 +49,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/reports', reportRouter);
 
-// catch 404 and forward to error handler
+// catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -50,7 +58,6 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   res.status(err.status || 500);
   res.render('error', { title: 'Error' });
 });
